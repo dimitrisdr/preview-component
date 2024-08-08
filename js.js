62111db4506646f75cb002aacdd7c147c39058f8
Dimitris_Dr <jidrist18@hotmail.com>
2024-08-08 12:23:03 +0300

    updated js code

--- js.js
+++ js.js
@@ -3,24 +3,25 @@ let btnContainer = document.querySelector('.btn-container')
 let authorInfoContainer = document.querySelector('.author-info-container')
 let btn = document.querySelector('.btn')
 
-console.log(shareContainer.parentElement)
-
-window.addEventListener('resize', ()=> {
-    console.log('reszing')
+function putSharecontainerToProperParent(){
     if (window.innerWidth >= 1000) {
         if (authorInfoContainer.contains(shareContainer)){
-            console.log('case 1')
             btnContainer.appendChild(shareContainer)
         }
     }else {
-        console.log('case 2')
         if(btnContainer.contains(shareContainer)){
             authorInfoContainer.appendChild(shareContainer)
         }
     }
+}
+
+window.addEventListener('resize', ()=> {
+    putSharecontainerToProperParent()
 })
 
 
 btn.addEventListener('click', ()=> {
     shareContainer.classList.toggle('hidden')
 })
+
+putSharecontainerToProperParent()
